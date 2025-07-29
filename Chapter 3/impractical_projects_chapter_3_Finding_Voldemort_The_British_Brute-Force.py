6e4470693c4b820075ac8eedfd10396b0649cba4
Ashish Manwar <ashishmanwar7@gmail.com>
2025-07-29 09:21:24 +0530

    Changes in brute force code

--- Chapter 3/impractical_projects_chapter_3_Finding_Voldemort_The_British_Brute-Force.py
+++ Chapter 3/impractical_projects_chapter_3_Finding_Voldemort_The_British_Brute-Force.py
@@ -1,13 +1,48 @@
 from itertools import permutations
 
-word = "tmvoordle"
+def main():
+    name = "tmvoordle"
 
-word_list = permutations(word, len(word))
-word_set =[]
+    volde_per = permutations(name)
+    perms = []
+    for letters in volde_per:
+        perms.append("".join(letters))
+    volde_per_set = set(perms)
+    print("Total Premuatations=", len(volde_per_set))
 
-for words in word_list:
-    dummy = ''.join(words)
-    if dummy not in word_set:
-        word_set.append(dummy)
+
+    file = open("2of4brif.txt")
+    word_list = file.read().split()
+
+    cv_map_word_list = cv_map(word_list)
+    print(len(cv_map_word_list))
+
+def cv_map(word_list):
+
+    vowels ="aeiou"
+    cv_map_word_list =[]
+
+    for word in word_list:
+        cv_map_word = ""
+        for letter in word:
+            if letter in vowels:
+                cv_map_word=cv_map_word + "v"
+            else:
+                cv_map_word=cv_map_word + "c"
+        cv_map_word_list.append(cv_map_word)
+        
+    
+    return cv_map_word_list
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+    
 
 
